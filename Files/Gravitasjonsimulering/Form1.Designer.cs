c8221d73a1c335445939e93f154ecf37468d8b58
daDevBoat <113507675+daDevBoat@users.noreply.github.com>
2022-11-16 17:00:44 +0100

    Add files via upload

---
+++ Files/Gravitasjonsimulering/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Gravitasjonsimulering
+{
+    partial class Gravitasjonssimulering
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.planetNumInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // planetNumInput
+            // 
+            this.planetNumInput.Location = new System.Drawing.Point(12, 12);
+            this.planetNumInput.Name = "planetNumInput";
+            this.planetNumInput.Size = new System.Drawing.Size(143, 23);
+            this.planetNumInput.TabIndex = 0;
+            this.planetNumInput.Text = "Skriv inn antall planeter";
+            this.planetNumInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 1;
+            // 
+            // Gravitasjonssimulering
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.planetNumInput);
+            this.Name = "Gravitasjonssimulering";
+            this.Text = "Gravitasjonssimulering";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox planetNumInput;
+        private Label label1;
+    }
+}
---
+++ Files/Gravitasjonsimulering/Form1.cs
@@ -0,0 +1,106 @@
+using System.Diagnostics;
+
+namespace Gravitasjonsimulering
+{
+    public class Planet
+    {
+        public string name;
+        public int masse;
+        public float[] pos;
+        public float fart;
+        public float fartVinkel;
+        public float[][] tyngde;
+        public float tyngdeX = 23;
+        public float tyngdeY;
+
+
+        public Planet(string inpName, int inpMasse, float[] inpPos, float inpFart, float inpFartVinkel)
+        {
+            name = inpName;
+            pos = inpPos;
+            fart = inpFart;
+            fartVinkel = inpFartVinkel;
+            masse = inpMasse;
+
+        }
+        public int massMethod()
+        {
+            return masse;
+        }
+    }
+
+
+
+    public partial class Gravitasjonssimulering : Form
+    {
+        public Gravitasjonssimulering()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void TestButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TestButton2_Click(object sender, EventArgs e)
+        {
+            Button button = new Button();
+            //button.Location(500, 100);
+            Controls.Add(button);
+            button.Location = new System.Drawing.Point(500, 100);
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Planet jorda = new Planet("Jorda", 10, new float[] { 100, 10 }, 10, 90);
+            Planet mars = new Planet("mars", 10, new float[] {200, 10}, 10, 90);
+            Planet sola = new Planet("sola", 10, new float[] {300, 10}, 10, 90);
+            Planet månen = new Planet("månen", 10, new float[] { 410, 10}, 10, 90);
+            //object[] planetListRaw = {jorda, mars, sola, månen};
+            List<object> planetList = new List<object>();
+            planetList.Add(jorda);
+            planetList.Add(mars);
+            planetList.Add(månen);
+            planetList.Add(sola);
+
+            foreach (Planet planet in planetList)
+            {
+                Console.WriteLine(planet);
+                Debug.WriteLine(planet);
+            }
+            jorda.pos = new float[] { 350, 400 };
+            jorda.pos[0] = 500;
+
+            int numOfPlanets = Convert.ToInt32(planetNumInput.Text);
+            Debug.WriteLine(numOfPlanets);
+            //planetList[0].massMethod();
+
+            //for (int i = 0; i < numOfPlanets; i++)
+            //{
+
+            //    Button button = new Button();
+            //    Controls.Add(button);
+            //    Object obj = planetList[i];
+            //    float[] pos = obj.pos;
+            //    obj.tyngdeX = 23;
+            //    button.Location = new System.Drawing.Point(pos[0], pos[1]);
+            //}
+
+
+
+        }
+        
+    }
+
+}
